23c4dff21bd13b7deafdb0c5befbcf52375722c2
K-M-Shehan <miweshshehan@gmail.com>
2025-03-26 11:34:53 +0530

    styled main

--- ReactFacts/src/components/Main.jsx
+++ ReactFacts/src/components/Main.jsx
@@ -1,11 +1,22 @@
+/**
+ * Challenge: Build out the main content section:
+ * 
+ * For now, skip 2 aspects of the design and we'll work on them later:
+ *  1. The colored bullets in teh list
+ *  2. The larger gray React logo on the side
+ */
+
 export default function Main() {
     return (
-        <ul>
-            <li>Was first release in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
